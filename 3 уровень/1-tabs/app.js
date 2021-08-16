d68b786c65f773dee608279d5d71ea6892f70eec
Alex <a.a.alexashkin@mail.ru>
2021-08-16 12:00:42 +0300

    Lesson_7_Tasks

---
+++ 3 уровень/1-tabs/app.js
@@ -0,0 +1,52 @@
+'use strict';
+const texts = {
+    text1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    text2: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.',
+    text3: 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.'
+};
+
+/* 
+1. Получите ссылку на .text, например с помощью querySelector
+2. Получите коллекцию, в которой хранятся все .nav-link, например с помощью querySelectorAll
+    2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
+    обработчик клика функцию clickHandler.
+*/
+document.querySelectorAll(".nav-link").forEach(function(elem){
+    elem.addEventListener("click", clickHandler);
+});
+
+/**
+ * Обработчик клика по .nav-link
+ * @param {MouseEvent} event 
+ */
+function clickHandler(event) {
+    // здесь вызывайте changeText и changeActiveClass, и передавайте
+    // им объект события.
+    changeActiveClass(event);
+    changeText(event);
+   
+}
+
+/**
+ * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
+ * на тот, по которому кликнули.
+ * @param {MouseEvent} event 
+ */
+let last_node = document.querySelector(".nav-link");
+function changeActiveClass(event) {
+    last_node.classList.remove("active");
+    event.target.classList.add("active");
+    last_node = event.target;
+}
+
+/**
+ * Эта фукнция должна читать текст (например через textContent) из 
+ * .nav-link по которому кликнули и в зависимости от этого в .text
+ * ставить соответствующий текст из texts.
+ * @param {MouseEvent} event 
+ */
+function changeText(event) {
+    /* console.log(`text${event.target.textContent.length-1}`) */
+    let txt = event.target.textContent;
+    document.querySelector('.text').textContent = texts[`text${txt[txt.length-1]}`];
+}
